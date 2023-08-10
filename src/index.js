4b7515f927685550baa8eb5251599b2d4f8920eb
Ramotshela <122275648+Ramotshela@users.noreply.github.com>
2023-08-10 05:10:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import renderApp from './render';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+renderApp(App);
